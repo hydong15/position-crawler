299d2d9402eba3fc705ed693f62461f8a7d62ebf
hgYim <hydong15@ajou.ac.kr>
2019-07-09 20:08:08 +0900

    Add spring boot project

---
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/PositionCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.positioncrawler.positioncrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionCrawlerApplication.class, args);
+	}
+
+}
