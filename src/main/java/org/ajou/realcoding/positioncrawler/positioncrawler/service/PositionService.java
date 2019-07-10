9394ed38e4341762e53bcf0a07146023350f8ee1
hgYim <hydong15@ajou.ac.kr>
2019-07-11 03:46:01 +0900

    Add getEncryptedSummonerId method in PositionService class

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/service/PositionService.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/service/PositionService.java
@@ -1,8 +1,17 @@
 package org.ajou.realcoding.positioncrawler.positioncrawler.service;
 
+import org.ajou.realcoding.positioncrawler.positioncrawler.api.DeveloperRiotgamesApiClient;
+import org.ajou.realcoding.positioncrawler.positioncrawler.domain.EncryptedSummonerId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PositionService {
+    @Autowired
+    DeveloperRiotgamesApiClient developerRiotgamesApiClient;
 
+    public String getEncryptedSummonerId(String summonerName) {
+        EncryptedSummonerId encryptedSummonerId = developerRiotgamesApiClient.requestEncryptedSummonerId(summonerName);
+        return encryptedSummonerId.getId();
+    }
 }
