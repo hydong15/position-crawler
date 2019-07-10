c766853e28e85e6fc9ae95c949f6e49166b31e4c
hgYim <hydong15@ajou.ac.kr>
2019-07-11 03:31:48 +0900

    Modify the package name

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/config/HttpConfig.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/config/HttpConfig.java
@@ -1,4 +1,4 @@
-package org.ajou.realcoding.positioncrawler.positioncrawler.api;
+package org.ajou.realcoding.positioncrawler.positioncrawler.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
