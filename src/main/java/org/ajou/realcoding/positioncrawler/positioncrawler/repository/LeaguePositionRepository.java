67bf41d8427ff760b2b43ca61d1b21423ac60009
hgYim <hydong15@ajou.ac.kr>
2019-07-11 03:59:26 +0900

    Add repository.LeaguePositionRepository.java

---
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/repository/LeaguePositionRepository.java
@@ -0,0 +1,14 @@
+package org.ajou.realcoding.positioncrawler.positioncrawler.repository;
+
+import org.ajou.realcoding.positioncrawler.positioncrawler.domain.LeaguePosition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class LeaguePositionRepository {
+    @Autowired
+    MongoTemplate mongoTemplate;
+}
