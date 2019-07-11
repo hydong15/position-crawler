18b045a5b22699463566e68fbc579c1c06c0d55e
hydong15 <48589661+hydong15@users.noreply.github.com>
2019-07-11 14:26:11 +0900

    Add league position crawling service and offer the LeaguePosition by summoner name (#1)
    * Insert league position in MongoDB
    
    * Find league position in MongoDB
    
    * Search in MongoDB if the league position already exist
    
    * Add updateLeaguePosition method in LeaguePositionRepository class

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/repository/LeaguePositionRepository.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/repository/LeaguePositionRepository.java
@@ -3,6 +3,8 @@ package org.ajou.realcoding.positioncrawler.positioncrawler.repository;
 import org.ajou.realcoding.positioncrawler.positioncrawler.domain.LeaguePosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,27 @@ import java.util.List;
 public class LeaguePositionRepository {
     @Autowired
     MongoTemplate mongoTemplate;
+
+    public boolean isExistLeaguePosition(List<LeaguePosition> leaguePosition) {
+        if (mongoTemplate.exists(Query.query(Criteria.where("summonerId").is(leaguePosition.get(0).getSummonerId())), LeaguePosition.class)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //WIP
+    public void updateLeaguePosition(List<LeaguePosition> leaguePosition) {
+        //mongoTemplate.update()
+    }
+
+    public void insertLeaguePosition(List<LeaguePosition> leaguePosition) {
+        mongoTemplate.insert(leaguePosition, LeaguePosition.class);
+    }
+
+    public List<LeaguePosition> findLeaguePosition(String encryptedSummonerId) {
+        List<LeaguePosition> leaguePositionList = mongoTemplate.find(Query.query(Criteria.where("summonerId").is(encryptedSummonerId)), LeaguePosition.class);
+        return leaguePositionList;
+    }
 }
