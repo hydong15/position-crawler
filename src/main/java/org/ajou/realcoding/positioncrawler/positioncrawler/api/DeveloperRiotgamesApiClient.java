31a4734060ef02c8f6f567416094d8e02bf7e27b
hgYim <hydong15@ajou.ac.kr>
2019-07-12 16:17:49 +0900

    Complete updateLeaguePosition method in LeaguePositionRepository class

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/api/DeveloperRiotgamesApiClient.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/api/DeveloperRiotgamesApiClient.java
@@ -5,7 +5,6 @@ import org.ajou.realcoding.positioncrawler.positioncrawler.domain.LeaguePosition
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +12,7 @@ import java.util.List;
 
 @Service
 public class DeveloperRiotgamesApiClient {
-    private final String appid = "RGAPI-0b63d3e8-55ad-467f-a1a8-2fbcd592bb87";
+    private final String appid = "RGAPI-805ea45b-c5bc-4a2b-af3d-e4f822ebb3dd";
     private final String developerRiotgamesSummonerUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={appid}";
     private final String developerRiotgamesLeagueUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={appid}";
 
--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/repository/LeaguePositionRepository.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/repository/LeaguePositionRepository.java
@@ -1,31 +1,44 @@
 package org.ajou.realcoding.positioncrawler.positioncrawler.repository;
 
+import com.mongodb.client.result.UpdateResult;
+import lombok.extern.slf4j.Slf4j;
 import org.ajou.realcoding.positioncrawler.positioncrawler.domain.LeaguePosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@Slf4j
 public class LeaguePositionRepository {
     @Autowired
     MongoTemplate mongoTemplate;
 
     public boolean isExistLeaguePosition(List<LeaguePosition> leaguePosition) {
-        if (mongoTemplate.exists(Query.query(Criteria.where("summonerId").is(leaguePosition.get(0).getSummonerId())), LeaguePosition.class)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return mongoTemplate.exists(Query.query(Criteria.where("summonerId").is(leaguePosition.get(0).getSummonerId())), LeaguePosition.class);
     }
 
-    //WIP
     public void updateLeaguePosition(List<LeaguePosition> leaguePosition) {
-        //mongoTemplate.update()
+        for (int i = 0; i < leaguePosition.size(); i++) {
+            Update update = new Update();
+            update.set("hotStreak", leaguePosition.get(i).isHotStreak());
+            update.set("wins", leaguePosition.get(i).getWins());
+            update.set("veteran", leaguePosition.get(i).isVeteran());
+            update.set("losses", leaguePosition.get(i).getLosses());
+            update.set("rank", leaguePosition.get(i).getRank());
+            update.set("leagueId", leaguePosition.get(i).getLeagueId());
+            update.set("inactive", leaguePosition.get(i).isInactive());
+            update.set("freshBlood", leaguePosition.get(i).isFreshBlood());
+            update.set("tier", leaguePosition.get(i).getTier());
+            update.set("leaguePoints", leaguePosition.get(i).getLeaguePoints());
+
+            UpdateResult result = mongoTemplate.updateFirst(Query.query(Criteria.where("summonerId").is(leaguePosition.get(i).getSummonerId()).and("queueType").is(leaguePosition.get(i).getQueueType())), update, LeaguePosition.class);
+            log.info("update " + i + ": " + result);
+        }
     }
 
     public void insertLeaguePosition(List<LeaguePosition> leaguePosition) {
@@ -33,7 +46,6 @@ public class LeaguePositionRepository {
     }
 
     public List<LeaguePosition> findLeaguePosition(String encryptedSummonerId) {
-        List<LeaguePosition> leaguePositionList = mongoTemplate.find(Query.query(Criteria.where("summonerId").is(encryptedSummonerId)), LeaguePosition.class);
-        return leaguePositionList;
+        return mongoTemplate.find(Query.query(Criteria.where("summonerId").is(encryptedSummonerId)), LeaguePosition.class);
     }
 }
