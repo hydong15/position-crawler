7b7928ffcb8e43a627153d4448f036bbbc17b26f
huigyun <48589661+hydong15@users.noreply.github.com>
2019-07-16 15:41:38 +0900

    Refactoring (#6)

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/service/PositionService.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/service/PositionService.java
@@ -26,15 +26,16 @@ public class PositionService {
     public List<LeaguePosition> getLeaguePositionBySummonerName(String summonerName) {
         String encryptedSummonerId = this.getEncryptedSummonerId(summonerName);
         List<LeaguePosition> leaguePositionList = developerRiotgamesApiClient.requestLeaguePosition(encryptedSummonerId);
+        this.setIdBySummonerNameAndQueueType(leaguePositionList);
+        leaguePositionRepository.insertOrUpdateLeaguePosition(leaguePositionList);
+        return leaguePositionRepository.findLeaguePosition(encryptedSummonerId);
+    }
 
-        //use summoner name and queuetype as id
+    public void setIdBySummonerNameAndQueueType(List<LeaguePosition> leaguePositionList) {
         for (LeaguePosition position : leaguePositionList) {
             position.setId(position.getSummonerName() + position.getQueueType());
         }
 
         log.info("leaguePositionList: " + leaguePositionList);
-
-        leaguePositionRepository.insertOrUpdateLeaguePosition(leaguePositionList);
-        return leaguePositionRepository.findLeaguePosition(encryptedSummonerId);
     }
 }
