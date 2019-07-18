888d830a9694ba94291ae9869737787909ef1ce5
hgYim <hydong15@ajou.ac.kr>
2019-07-18 23:07:26 +0900

    Change api key

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/api/DeveloperRiotgamesApiClient.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/api/DeveloperRiotgamesApiClient.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 public class DeveloperRiotgamesApiClient {
-    private final String appid = "RGAPI-4de1631b-8468-4ab6-b4ab-b48c7a54c534";
+    private final String appid = "RGAPI-8874a7cd-ad68-4f60-8ec8-e975f32d2f66";
     private final String developerRiotgamesSummonerUri = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={appid}";
     private final String developerRiotgamesLeagueUri = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={appid}";
 
@@ -20,11 +20,11 @@ public class DeveloperRiotgamesApiClient {
     private
     RestTemplate restTemplate;
 
-    public EncryptedSummonerId requestEncryptedSummonerId (String summonerName) {
+    public EncryptedSummonerId requestEncryptedSummonerId(String summonerName) {
         return restTemplate.exchange(developerRiotgamesSummonerUri, HttpMethod.GET, null, EncryptedSummonerId.class, summonerName, appid).getBody();
     }
 
-    public List<LeaguePosition> requestLeaguePosition (String encryptedSummonerId) {
+    public List<LeaguePosition> requestLeaguePosition(String encryptedSummonerId) {
         return restTemplate.exchange(developerRiotgamesLeagueUri, HttpMethod.GET, null, new ParameterizedTypeReference<List<LeaguePosition>>() {}, encryptedSummonerId, appid).getBody();
     }
 }
